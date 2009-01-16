161e0f09531cbd3bf634bf801700f631d1337fe7
Thomas Aylott <oblivious+git@subtlegradient.com>
2009-01-16 12:17:59 -0500

    Updated template

--- sx.js
+++ sx.js
@@ -17,13 +17,22 @@ var TEMPLATE = {
     content_placer:'{CONTENT}',
     
     html:'\
-    <div class="container">\
-        <p>Header</p>\
-        <hr>\
-        {CONTENT}\
-        <hr>\
-        <p>Footer</p>\
+<div class="container">\
+    <p class="box">Header</p>\
+    <div class="clearfix">\
+        <div class="span-16 first colborder">\
+            {CONTENT}\
+        </div>\
+        <div class="span-6 last small">\
+            <p>This sidebar isn\'t in the document source because it\'s semantically not part of the document.</p>\
+            <p>This could be for ads or whatever.</p>\
+            <hr>\
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>\
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>\
+        </div>\
     </div>\
+    <p class="box">Footer</p>\
+</div>\
     '
 };
 
